401430e453642f5946f56378e292106d4e6c4bba
Gnezd <ycdzeng@gmail.com>
2024-03-29 19:05:16 +0800

    Fixing a few mysterious fields and ForceMap class building

--- lib.rb
+++ lib.rb
@@ -1,15 +1,15 @@
 class ARDF
-  attr_reader :pos, :name, :size, :misc, :entries, :data
+  attr_reader :pos, :type, :size, :misc, :entries, :data
   def initialize(raw, pos) # Stupidly preserving the whole raw as pointer is used
     @data = {}
 
     @pos = pos
-    null, @size, @name, @misc = header(raw, pos)
+    null, @size, @type, @misc = header(raw, pos)
     pos += 16
     raise "Size zero at #{pos}" unless @size > 0
     # puts "#{@name} at #{@pos} with size #{@size}"
 
-    case @name
+    case @type
     when 'ARDF'
       # puts "ARDF begins"
     when 'FTOC', 'TTOC', 'VOLM', 'VTOC', 'IBOX' # is_an? TOC
@@ -82,7 +82,7 @@ class ARDF
       @data[:dim] = raw[pos..pos+7].unpack "LL"
       pos += 8
       pos += 24 # blank
-      @data[:unknown] = raw[pos..pos+23].unpack "qqq" # 3x 64b float
+      @data[:unknown] = raw[pos..pos+23].unpack "ddd" # 3x 64b float, xyz grid size?
       pos += 24
       @data[:unknown] += raw[pos..pos+95].unpack "q*" # 3x32B blocks
       pos += 96
@@ -92,10 +92,9 @@ class ARDF
 
     # Volume channel?
     when 'VCHN'
-      @data[:title] = raw[pos..pos+31]
+      @data[:title] = raw[pos..pos+31].gsub("\x0", "")
       pos += 32
       @data[:unknown] = raw[pos .. @pos+@size-1]
-
 
     when 'XDEF'
       pos += 4 # \0 x4
@@ -116,26 +115,29 @@ class ARDF
     when 'XDAT'
       raise "not of size 80!" unless @size == 80
       pixel_n, row, col, dbfl_n, x, y, mystery1, mystery2, nan1, nan2 = raw[pos..pos+63].unpack("LLLLDDDDDD")
+      # Consult XDEF to understand these better
       @data[:xdat] = [pixel_n, row, col, dbfl_n, x, y, mystery1, mystery2, nan1, nan2]
       pos += size
     
+    # Looks like "Image data"
     when 'IDAT'
-      @data[:unknown] = raw[pos..pos+@size-17].unpack "F*"
+      @data[:line] = raw[pos..pos+@size-17].unpack "F*"
 
     # Volume offset?
     when 'VOFF'
-      @data[:leading_pt], @data[:line], @data[:unknown], @data[:ptr] = raw[pos..pos+23].unpack "LLqq"
+      @data[:leading_pt], @data[:line], @data[:first_pt_x], @data[:vset_ptr] = raw[pos..pos+23].unpack "LLqq"
 
     when 'VSET'
-      @data[:ptnum], @data[:line], @data[:remaining], @data[:unknown], @data[:last], @data[:next] = raw[pos..pos+31].unpack "LLLLqq"
+      @data[:ptnum], @data[:y], @data[:x], @data[:unknown], @data[:last], @data[:next] = raw[pos..pos+31].unpack "LLLLqq"
+      # The unknown seems to be always 2
 
     when 'VNAM'
-      @data[:ptnum], @data[:line], @data[:remaining], text_length = raw[pos..pos+15].unpack "LLLL"
+      @data[:ptnum], @data[:y], @data[:x], text_length = raw[pos..pos+15].unpack "LLLL"
       pos += 16
       @data[:title] = raw[pos..pos+text_length-1]
 
     when 'VDAT'
-      @data[:ptnum], @data[:line], @data[:point], datasize, @data[:channel], pt0, pt1, pt2 = raw[pos..pos+39].unpack "LLLLqLLL@@@@"
+      @data[:ptnum], @data[:y], @data[:x], datasize, @data[:channel], pt0, pt1, pt2 = raw[pos..pos+39].unpack "LLLLqLLL@@@@"
       pos += 40
       # puts "pt012: #{pt0} #{pt1} #{pt2}"
       @data[:segments] = Array.new(3)
@@ -147,7 +149,7 @@ class ARDF
 
 
     else
-      raise "#{@name} unrecognized at #{pos}! \n #{raw[pos..pos+15]}"
+      raise "#{@type} unrecognized at #{pos}! \n #{raw[pos..pos+15]}"
     end
   end
 
@@ -159,20 +161,22 @@ class ARDF
 
   def inspect
 #  attr_reader :pos, :name, :size, :misc, :entries, :data
-    report = "#{@name} at position #{@pos} size #{@size}"
-    report += ", containing #{@entries.size} entries" if @entries.is_a? Array
-    report += "Data hash keys: [#{@data.keys.join(", ")}]" if @data.size > 0 
+    report = "#{@type} at position #{@pos} size #{@size}"
+    if @entries.is_a? Array
+      report += ", containing #{@entries.size} entries\n" 
+      report += (@entries.map{|entry| "  #{entry.inspect}"}).join("\n") # Get nesty
+    end
+    report += ", data hash keys: [#{@data.keys.join(", ")}]" if @data.size > 0 
     report
   end
 end
 
 
-class ForceCurveMap
+class ForceMap
   attr_accessor :name
-  attr_reader :channels, :width, :height, :lines
+  attr_reader :channels, :width, :height
   def initialize(fin)
-    raw = File.open
-    raw = File.open(fin, 'rb').read
+    raw = File.open(fin, 'rb').read.freeze
     ptr = 0
 
     @ardf = []
@@ -181,7 +185,23 @@ class ForceCurveMap
       ptr += new_obj.size
       @ardf.push new_obj
     end
+    raw = nil # Trash it
+    
+    @mlov_index = @ardf.index {|o| o.type == 'MLOV'}
+    
+    @width, @height = @ardf.find{|o| o.type == 'VDEF'}.data[:dim]
+    @channels = @ardf.filter {|o| o.type == 'VCHN'}.map {|o| o.data[:title]}
 
+  end
 
+  def at(x, y, segment = nil, ch = nil)
+    # Brute search for now
+    data = @ardf.filter {|o| o.type == 'VDAT' && o.data[:x] == x && o.data[:y] == y}
+    data.filter! {|o| o.data[:channel] == ch} if ch
+      if segment
+        return data.map{|o| o.data[:segments][segment]}
+      else
+        return data.map{|o| o.data[:segments]}
+      end
   end
 end
