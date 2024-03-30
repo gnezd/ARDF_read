fcc3ef2cd54ce7e307501cd46ef9ed169550b380
YC Dzeng <ycdzneg@gmail.com>
2024-03-30 09:41:14 +0800

    compartmentize test

--- test.rb
+++ test.rb
@@ -1,17 +1,20 @@
 require 'pry'
 require './lib'
 
-fin = './testdata/ForceMap001.ARDF'
-raw = File.open(fin, 'rb').read
-ptr = 0
-objects = []
+def raw_load(fin)
+  raw = File.open(fin, 'rb').read
+  ptr = 0
+  objects = []
 
-while ptr < raw.size-1
-  new_obj = ARDF.new(raw, ptr)
-  ptr += new_obj.size
-  objects.push new_obj
+  while ptr < raw.size-1
+    new_obj = ARDF.new(raw, ptr)
+    ptr += new_obj.size
+    objects.push new_obj
+  end
+  File.open("object_list.txt", "w") {|f| f.puts (0..objects.size-1).map{|i| "#{i}: #{objects[i].inspect}"}}
 end
-File.open("object_list.txt", "w") {|f| f.puts (0..objects.size-1).map{|i| "#{i}: #{objects[i].inspect}"}}
 
+fin = './testdata/ForceMap001.ARDF'
+ardf1 = ForceMap.new fin
 
 binding.pry
